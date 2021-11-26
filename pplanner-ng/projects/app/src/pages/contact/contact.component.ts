cdd35cfad448ba772dae53d23050969191a10d8c
laurent.parrot <laurent.parrot78@gmail.com>
2021-11-26 12:50:51 +0100

    Ajout du formulaire de contact

--- pplanner-ng/projects/app/src/pages/contact/contact.component.ts
+++ pplanner-ng/projects/app/src/pages/contact/contact.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import {FormBuilder, FormGroup, Validators} from '@angular/forms'
 
 @Component({
 	selector: 'app-contact',
 	templateUrl: './contact.component.html',
-	styleUrls: [ './contact.component.scss' ],
+	styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
 
-	constructor () {
+	form: FormGroup
+
+	constructor(fb: FormBuilder) {
+		this.form = fb.group({
+			name: ['', Validators.required],
+			email: ['', Validators.compose([Validators.required, Validators.email])],
+			message: ['', Validators.required],
+		})
 	}
 
-	ngOnInit (): void {
+	ngOnInit(): void {
 	}
 
+	contact() {
+		if (this.form.valid) {
+			console.log(this.form.value)
+		}
+	}
 }
